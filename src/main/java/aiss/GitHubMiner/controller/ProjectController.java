5a3cc9e69a7b0990f497aa024e5f5c05f7daeead
JAIME DIAZ <jaidiavil@alum.us.es>
2023-06-18 17:47:36 +0200

    Mas

--- src/main/java/aiss/GitHubMiner/controller/ProjectController.java
+++ src/main/java/aiss/GitHubMiner/controller/ProjectController.java
@@ -13,22 +13,24 @@ public class ProjectController {
 
     final String gitMinerUri= "http://localhost:8080/gitminer/v1/projects";
 
-
     //POST http://localhost:8082/githubminer/{owner}/{repoName}
     @Autowired
     GitHubService service;
 @PostMapping("/{owner}/{repo}")
 @ResponseStatus(HttpStatus.CREATED)
 public ProjectGitMiner createProject(@PathVariable String owner, @PathVariable String repo){
-    Project p = service.findAProjectByOwnerAndRepo(owner,repo);
-    ProjectGitMiner res = service.createProject(p);
+    Project project = service.findProjectByOwnerAndRepo(owner,repo);
+    ProjectGitMiner res = service.createProject(project);
+
     return res;
 }
 
     // GET http://localhost:8082/api/projects/{owner}/{repo}
     @GetMapping("/{owner}/{repo}")
     public Project findOne(@PathVariable String owner, @PathVariable String repo){
-        return service.findAProjectByOwnerAndRepo(owner,repo);
+
+        return service.findProjectByOwnerAndRepo(owner,repo);
+
     }
 
 }
